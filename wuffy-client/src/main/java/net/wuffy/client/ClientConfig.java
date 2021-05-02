8ea01c1bcf88444e7bccb6406026985e5264398d
NgLoader <nilsgereke1@gmail.com>
2021-05-02 22:43:12 +0200

    feat: removed client

--- wuffy-client/src/main/java/net/wuffy/client/ClientConfig.java
+++
@@ -1,8 +0,0 @@
-package net.wuffy.client;
-
-import net.wuffy.common.config.Config;
-import net.wuffy.core.CoreConfig;
-
-@Config(path = "./wuffy/client.json", sourcePath = "/config/client.json")
-public class ClientConfig extends CoreConfig {
-}
